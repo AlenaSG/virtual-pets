76b65f2b6fc522454a36f2f90e7447f8fcdb3420
Witty Chang <wcc1213@gmail.com>
2017-07-13 14:49:43 -0700

    Add functionality to find Person object based on id and run test to pass.

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,6 +1,11 @@
+import org.sql2o.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Person {
   private String name;
   private String email;
+  private int id;
 
   public Person(String name, String email) {
     this.name = name;
@@ -15,6 +20,10 @@ public class Person {
     return email;
   }
 
+  public int getId() {
+    return id;
+  }
+
   @Override
   public boolean equals(Object otherPerson) {
     if (!(otherPerson instanceof Person)) {
@@ -25,4 +34,33 @@ public class Person {
               this.getEmail().equals(newPerson.getEmail());
     }
   }
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO persons (name, email) VALUES (:name, :email)";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("name", this.name)
+        .addParameter("email", this.email)
+        .executeUpdate()
+        .getKey();
+    }
+  }
+
+  public static List<Person> all() {
+    String sql = "SELECT * FROM persons";
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(Person.class);
+    }
+  }
+
+  public static Person find(int id) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM persons where id=:id";
+      Person person = con.createQuery(sql)
+        .addParameter("id", id)
+        .executeAndFetchFirst(Person.class);
+      return person;
+    }
+  }
+
 }
--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -1,7 +1,11 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+import org.sql2o.*;
 
 public class PersonTest {
+
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
 
   @Test
   public void person_instantiatesCorrectly_true() {
@@ -26,6 +30,40 @@ public class PersonTest {
     Person firstPerson = new Person("Henry", "henry@henry.com");
     Person anotherPerson = new Person("Henry", "henry@henry.com");
     assertTrue(firstPerson.equals(anotherPerson));
+  }
+
+  @Test
+  public void save_insertsObjectIntoDatabase_Person() {
+    Person testPerson = new Person("Henry", "henry@henry.com");
+    testPerson.save();
+    assertTrue(Person.all().get(0).equals(testPerson));
+  }
+
+  @Test
+  public void all_returnsAllInstancesOfPerson_true() {
+    Person firstPerson = new Person("Henry", "henry@henry.com");
+    firstPerson.save();
+    Person secondPerson = new Person("Harriet", "harriet@harriet.com");
+    secondPerson.save();
+    assertEquals(true, Person.all().get(0).equals(firstPerson));
+    assertEquals(true, Person.all().get(1).equals(secondPerson));
+  }
+
+  @Test
+  public void save_assignsIdToObject() {
+    Person testPerson = new Person("Henry", "henry@henry.com");
+    testPerson.save();
+    Person savedPerson = Person.all().get(0);
+    assertEquals(testPerson.getId(), savedPerson.getId());
+  }
+
+  @Test
+  public void find_returnsPersonWithSameId_secondPerson() {
+    Person firstPerson = new Person("Henry", "henry@henry.com");
+    firstPerson.save();
+    Person secondPerson = new Person("Harriet", "harriet@harriet.com");
+    secondPerson.save();
+    assertEquals(Person.find(secondPerson.getId()), secondPerson);
   }
 
 }
